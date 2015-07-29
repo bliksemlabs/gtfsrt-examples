819c238fc9b89846a21393af706873b4d7f10f8b
Stefan de Konink <stefan@konink.de>
2015-07-29 20:51:12 +0200

    For clients that can't handle merged feeds, split them.
    https://github.com/google/transit/pull/7

---
+++ split_by_entitytype.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+
+import sys
+from gtfs_realtime_pb2 import FeedMessage
+from google.protobuf.text_format import MessageToString
+
+with open(sys.argv[1], "rb") as f:
+    feedmessage = FeedMessage()
+    feedmessage.ParseFromString(f.read())
+    trip_updates = FeedMessage()
+    vehicles = FeedMessage()
+    alerts = FeedMessage()
+
+    trip_updates.header.CopyFrom(feedmessage.header)
+    vehicles.header.CopyFrom(feedmessage.header)
+    alerts.header.CopyFrom(feedmessage.header)
+
+    for feedentity in feedmessage.entity:
+        if feedentity.HasField('is_deleted') and feedentity.is_deleted == True:
+            continue
+
+        elif feedentity.HasField('trip_update'):
+            e = trip_updates.entity.add()
+            e.CopyFrom(feedentity)
+
+        elif feedentity.HasField('vehicle'):
+            e = vehicles.entity.add()
+            e.CopyFrom(feedentity)
+
+        elif feedentity.HasField('alert'):
+            e = alerts.entity.add()
+            e.CopyFrom(feedentity)
+
+
+    with open('trip_updates.pb', 'wb') as t:
+        t.write(trip_updates.SerializeToString())
+
+    with open('vehicles.pb', 'wb') as v:
+        v.write(vehicles.SerializeToString())
+
+    with open('alerts.pb', 'wb') as a:
+        a.write(alerts.SerializeToString())
+
